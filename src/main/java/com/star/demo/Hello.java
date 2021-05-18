fc62e3a39c80650b13e5f7e15dde5276e69fa251
tom_glb <goodMorning_glb@star.com>
2021-05-18 12:29:42 +0800

    使用插件忽略文件大小写，2能成功吗？

---
+++ src/main/java/com/star/demo/Hello.java
@@ -0,0 +1,14 @@
+package com.star.demo;
+
+/**
+ * @Auther: zhangskystar
+ * @Date: 2021-05-18-05-18-11:43
+ * @Description: com.star.demo
+ * @Version: 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("test maven git");
+        System.out.println("使用插件的方式忽略文件");
+    }
+}
---
+++ src/test/java/com/star/demo/HelloTest.java
@@ -0,0 +1,13 @@
+package com.star.demo;
+
+/**
+ * @Auther: zhangskystar
+ * @Date: 2021-05-18-05-18-11:46
+ * @Description: com.star.demo
+ * @Version: 1.0
+ */
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
